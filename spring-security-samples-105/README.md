3cebbc36c23534591df57efd4d2d61beebcbd058
clinker <clinker@163.com>
2020-07-28 19:38:31 +0800

    105

---
+++ spring-security-samples-105/README.md
@@ -0,0 +1,11 @@
+# spring-security-samples-105
+示例105。
+
+# 说明
+## 1. 设置URI是否需要鉴权
+  - 首页接口（`/`）不需要鉴权
+  - hello接口（`/hello`）需要鉴权
+
+
+## 2. 路径匹配规则
+  - `AntPathRequestMatcher`是关键
---
+++ spring-security-samples-105/src/main/java/security105/config/SpringSecurityConfig.java
@@ -0,0 +1,43 @@
+package security105.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Spring security配置。
+ */
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// 创建多个内存用户
+		auth.inMemoryAuthentication()
+				.withUser("zhangsan")// 用户张三
+				.password(passwordEncoder.encode("111111"))
+				.roles("")// 本例不需要角色
+				.and()
+				.withUser("lisi")// 用户李四
+				.password(passwordEncoder.encode("222222"))
+				.roles("");// 本例不需要角色
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/")
+				.permitAll()// 不需要鉴权
+				.anyRequest()
+				.authenticated()// 所有其他请求都需要鉴权，会自动排除登录和注销接口
+				.and()
+				.formLogin();
+	}
+
+}
