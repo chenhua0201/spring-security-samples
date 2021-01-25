6ea1639234540ff9412234171c9e672c07673359
Chen Hua <clinker@163.com>
2021-01-25 09:08:50 +0800

    feat: spring boot升级到2.4.2

--- spring-security-samples-oauth2-resource-server/src/main/java/security/resource/config/EnhancedUserAuthenticationConverter.java
+++ spring-security-samples-oauth2-resource-server/src/main/java/security/resource/config/EnhancedUserAuthenticationConverter.java
@@ -7,10 +7,8 @@ import java.util.Map;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 /**
  * 按照spring-security-samples-oauth2-authz-server的TokenEnhancer的格式，读取额外的token信息。
@@ -23,9 +21,9 @@ class EnhancedUserAuthenticationConverter extends DefaultUserAuthenticationConve
 	private final String PASSWORD_NA = "N/A";
 
 	@Override
-	public Authentication extractAuthentication(Map<String, ?> map) {
+	public Authentication extractAuthentication(final Map<String, ?> map) {
 		if (map.containsKey(USERNAME)) {
-			final Collection<GrantedAuthority> authorities = getAuthorities(map);
+			final Collection<? extends GrantedAuthority> authorities = getAuthorities(map);
 
 			final String userId = (String) map.get("userId");
 			final String username = (String) map.get("username");
@@ -34,21 +32,6 @@ class EnhancedUserAuthenticationConverter extends DefaultUserAuthenticationConve
 			return new UsernamePasswordAuthenticationToken(user, PASSWORD_NA, authorities);
 		}
 		return null;
-	}
-
-	private Collection<GrantedAuthority> getAuthorities(Map<String, ?> map) {
-		if (!map.containsKey(AUTHORITIES)) {
-			return defaultAuthorities;
-		}
-		final Object authorities = map.get(AUTHORITIES);
-		if (authorities instanceof String) {
-			return AuthorityUtils.commaSeparatedStringToAuthorityList((String) authorities);
-		}
-		if (authorities instanceof Collection) {
-			return AuthorityUtils.commaSeparatedStringToAuthorityList(
-					StringUtils.collectionToCommaDelimitedString((Collection<?>) authorities));
-		}
-		throw new IllegalArgumentException("Authorities must be either a String or a Collection");
 	}
 
 }
